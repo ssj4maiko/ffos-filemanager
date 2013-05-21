97a9fe326518ff42203a1466a7a7b72180f9e5f0
ssj4maiko <ssj4maiko@gmail.com>
2013-05-21 00:23:20 -0300

    now scanning all files

--- file_manager.js
+++ file_manager.js
@@ -52,7 +52,8 @@ var FileDB = (function(){
 			if(!fdb.directory[father]){
 				fdb.directory[father] = {};
 			}
-			if(findDirectory(name,father) !== null)
+			var id = findDirectory(name,father);
+			if(id !== null)
 				return id;
 
 			var data = {
@@ -61,25 +62,25 @@ var FileDB = (function(){
 				,directory : true
 				,hidden : name.indexOf('.') === 0
 			};
-			fdb.directory[father].push(data);
+			fdb.directory[father][data.id] = data;
 			return data.id;
 		}
 		function pushFile(name,father,file){
 			if(!fdb.directory[father]){
-				fdb.directory[father] = [];
+				fdb.directory[father] = {};
 			}
 
 			var data = {
 				 name : name
-				//,id : fdb.index++
+				,id : fdb.index++
 				,directory : false
 				,hidden : name.indexOf('.') === 0
 				,extension : name.split('.').pop()
 				,type : file.type
 				,size : file.size
-				,timestamp : file.s
+				,timestamp : file.lastModifiedDate
 			}
-			fdb.directory[father].push(data);
+			fdb.directory[father][data.id] = data;
 			return data.id;
 		};
 		this.registerPath = function(file){
@@ -90,7 +91,7 @@ var FileDB = (function(){
 			for(i in path){
 				father = pushDirectory(path[i],father);
 			}
-			pushFile(path[length+1],father,file);
+			pushFile(path[length],father,file);
 		}
 		//alert(fdb.storage);
 		this.directory = Object.create(null);
@@ -99,11 +100,19 @@ var FileDB = (function(){
       var cursor = fdb.storage.enumerate();
       cursor.onsuccess = function(){
       	var file = cursor.result;
-      	if(file){
-      		fdb.registerPath(file);
-      	} else {
-      		debug();
-      	}
+      	document.body.textContent = fdb.index;
+      	try{
+	      	if(file){
+	      		fdb.registerPath(file);
+	    		cursor.continue();
+	      	} else {
+	      		alert('done');
+	      		//debug();
+	      	}
+	    }
+	    catch(e){
+	    	alert(e);
+	    }
       };
 	}
 
@@ -117,83 +126,32 @@ FileDB.UNMOUNTED = 'unmounted'; // Unavailable because card unmounted
 FileDB.CLOSED = 'closed';       // Unavailalbe because FileDB has closed
 
 var fdb = new FileDB();
-function debug(){
-	document.write = print_r(fdb.directory,true);
+function log(txt){
+	document.write(txt+'<br />');
 }
-function print_r (array, return_val) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: Michael White (http://getsprink.com)
-  // +   improved by: Ben Bryan
-  // +      input by: Brett Zamir (http://brett-zamir.me)
-  // +      improved by: Brett Zamir (http://brett-zamir.me)
-  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // -    depends on: echo
-  // *     example 1: print_r(1, true);
-  // *     returns 1: 1
-  var output = '',
-    pad_char = ' ',
-    pad_val = 4,
-    d = window.document,
-    getFuncName = function (fn) {
-      var name = (/\W*function\s+([\w\$]+)\s*\(/).exec(fn);
-      if (!name) {
-        return '(Anonymous)';
-      }
-      return name[1];
-    },
-    repeat_char = function (len, pad_char) {
-      var str = '';
-      for (var i = 0; i < len; i++) {
-        str += pad_char;
-      }
-      return str;
-    },
-    formatArray = function (obj, cur_depth, pad_val, pad_char) {
-      if (cur_depth > 0) {
-        cur_depth++;
-      }
-
-      var base_pad = repeat_char(pad_val * cur_depth, pad_char);
-      var thick_pad = repeat_char(pad_val * (cur_depth + 1), pad_char);
-      var str = '';
-
-      if (typeof obj === 'object' && obj !== null && obj.constructor && getFuncName(obj.constructor) !== 'PHPJS_Resource') {
-        str += 'Array\n' + base_pad + '(\n';
-        for (var key in obj) {
-          if (Object.prototype.toString.call(obj[key]) === '[object Array]') {
-            str += thick_pad + '[' + key + '] => ' + formatArray(obj[key], cur_depth + 1, pad_val, pad_char);
-          }
-          else {
-            str += thick_pad + '[' + key + '] => ' + obj[key] + '\n';
-          }
-        }
-        str += base_pad + ')\n';
-      }
-      else if (obj === null || obj === undefined) {
-        str = '';
-      }
-      else { // for our "resource" class
-        str = obj.toString();
-      }
-
-      return str;
-    };
-
-  output = formatArray(array, 0, pad_val, pad_char);
-
-  if (return_val !== true) {
-    if (d.body) {
-      this.echo(output);
-    }
-    else {
-      try {
-        d = XULDocument; // We're in XUL, so appending as plain text won't work; trigger an error out of XUL
-        this.echo('<pre xmlns="http://www.w3.org/1999/xhtml" style="white-space:pre;">' + output + '</pre>');
-      } catch (e) {
-        this.echo(output); // Outputting as plain text may work in some plain XML
-      }
-    }
-    return true;
-  }
-  return output;
+function debug(){
+	for(var i in fdb.directory){
+		var temp = fdb.directory[i];
+		log(i+' = {');
+		if(typeof temp == 'object'){
+			for(var j in temp){
+				var temp2 = temp[j];
+				log('---- '+j+' = {');
+				if(typeof temp2 == 'object'){
+					for(var k in temp2){
+						var temp3 = temp2[k];
+						log('-------- '+k+' = {');
+						log('-------- '+temp3);
+						log('-------- }');
+					}
+				} else {
+					log('---- '+temp2);
+				}
+				log('---- }');
+			}
+		} else {
+			log(temp);
+		}
+		log('}');
+	}
 }
